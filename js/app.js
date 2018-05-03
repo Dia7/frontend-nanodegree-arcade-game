959afcd64e05929963479e0b7bee64f5a395e050
Diana Ivanova Ratcheva <diana.ratcheva@gmail.com>
2018-05-03 14:01:32 +0300

    Create 3 Enemy objects + Add hendleInput() method to the Player.prototype

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 let yEnemies = [60, 140, 220, 140, 60, 210];
 
 // Enemies our player must avoid
-var Enemy = function(x, y, speed) {
+const Enemy = function(x, y, speed, sprite) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -30,6 +30,7 @@ Enemy.prototype.update = function(dt) {
             this.x += dt * 15 * this.speed * Math.random();
         } else {
             i = Math.random() * yEnemies.length | 0 + 0;
+
             this.y = yEnemies[i];
             this.x = -100;
         }
@@ -43,13 +44,13 @@ Enemy.prototype.render = function() {
 
 // Now write your own player class
 const Player = function (x, y){
-    this.sprite = 'images/Gem Blue.png';
+    this.sprite = 'images/char-pink-girl.png';
     this.x = x;
     this.y = y;
 }
 // This class requires an update(), render() and
 // a handleInput() method.
-// Add updade method to Player constructor fn.
+// Add updade() method to Player constructor fn.
 Player.prototype.update = function(dt) {
     //Return the player back once they hit water: With delay
     if (this.y < 0) {
@@ -65,20 +66,33 @@ Player.prototype.update = function(dt) {
         }, 600);
 }};
 
-// Add render method to Player constructor fn.
+// Add render() method to Player constructor fn.
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Add handleInput() method to Player constructor fn.
-
+Player.prototype.handleInput = function(key) {
+     if (key === 'left' && this.x > 0) {
+            this.x -= 100;
+        } else if (key === 'right' && this.x < 400) {
+            this.x += 100;
+        } else if (key === 'up') {
+            this.y -= 80;
+        } else if (key === 'down' && this.y < 380) {
+            this.y += 80;
+        }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-const allEnemies = [];
+let enemy1 = new Enemy(-100, 60, 20, this.sprite);
+let enemy2 = new Enemy(-100, 220, 40, this.sprite);
+let enemy3 = new Enemy(-100, 140, 10, this.sprite);
+const allEnemies = [enemy1, enemy2, enemy3];
 
 // Place the player object in a variable called player
-const player = new Player(30, 60);
+const player = new Player(200, 400);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
